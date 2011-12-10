829d1885aa0780284ca35b2bb570ed00dbfe8c99
kocsis.antal <kocsis.antal@8393cccc-66b0-f440-f94a-1bbcd1765e9f>
2011-12-10 18:35:05 +0000

    NEW: NotificationService bevezetése

--- oldSchoolSMS/src/hu/anti/android/oldSchoolSms/AbstractSmsActivity.java
+++ oldSchoolSMS/src/hu/anti/android/oldSchoolSms/AbstractSmsActivity.java
@@ -1,11 +1,7 @@
 package hu.anti.android.oldSchoolSms;
 
-import java.util.Date;
-
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -66,30 +62,5 @@ public abstract class AbstractSmsActivity extends Activity {
     protected void setText(int id, String text) {
 	TextView view = (TextView) findViewById(id);
 	view.setText(text);
-    }
-
-    protected Uri putNewSmsToDatabase(ContentResolver contentResolver, String address, String body, String type, String status) {
-	// Create SMS row
-	ContentValues values = new ContentValues();
-
-	values.put(Sms.Fields.ADDRESS, address);
-	values.put(Sms.Fields.DATE, "" + new Date().getTime());
-	values.put(Sms.Fields.READ, Sms.Other.MESSAGE_IS_READ);
-	values.put(Sms.Fields.STATUS, status);
-	values.put(Sms.Fields.TYPE, type);
-	values.put(Sms.Fields.BODY, body);
-
-	// Push row into the SMS table
-	Uri uri = contentResolver.insert(Uri.parse(Sms.Uris.SMS_URI_BASE), values);
-	// FIXME reupdate...
-	contentResolver.update(uri, values, null, null);
-
-	Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-	if (cursor != null && cursor.moveToFirst()) {
-	    Sms sms = Sms.parseSms(cursor);
-	    Log.d("OldSchoolSMS", "Inserted new SMS objet: [" + sms + "]");
-	}
-
-	return uri;
     }
 }
