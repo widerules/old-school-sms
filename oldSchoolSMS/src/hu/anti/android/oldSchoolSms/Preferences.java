53303a58a5f12f58f420985697d6b2cc6c7b1f6c
kocsis.antal <kocsis.antal@8393cccc-66b0-f440-f94a-1bbcd1765e9f>
2012-02-23 11:19:23 +0000

    NEW: extractPattern a preferences-ben van kezelve, egy helyen

--- oldSchoolSMS/src/hu/anti/android/oldSchoolSms/Preferences.java
+++ oldSchoolSMS/src/hu/anti/android/oldSchoolSms/Preferences.java
@@ -1,10 +1,21 @@
 package hu.anti.android.oldSchoolSms;
 
+import java.util.Locale;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class Preferences {
+
+    public static final String DEFAULT_SOUND = "DEFAULT_SOUND";
+
+    public static final String VIBRATOR_PATTERN = "vibratorPattern";
+    public static final String DefaultVibratorPattern = "500,200,300";
+
+    public static final String DELIVERY_VIBRATOR_PATTERN = "deliveryVibratorPattern";
+    public static final String DefaultDeliveryVibratorPattern = "500";
 
     private final SharedPreferences sharedPrefs;
 
@@ -25,11 +36,20 @@ public class Preferences {
     }
 
     public String getNotificationSound() {
-	return sharedPrefs.getString("notificationSound", "DEFAULT_SOUND");
+	return sharedPrefs.getString("notificationSound", DEFAULT_SOUND);
     }
 
-    public String getVibratorPattern() {
-	return sharedPrefs.getString("vibratorPattern", "500,200,300");
+    public long[] getVibratorPattern() {
+	long[] pattern;
+	try {
+	    pattern = extractPattern(sharedPrefs.getString(VIBRATOR_PATTERN, DefaultVibratorPattern));
+	} catch (Exception e) {
+	    Log.e("OldSchoolSMS", "Wrong vibration pattern: " + e.getLocalizedMessage());
+
+	    pattern = extractPattern(Preferences.DefaultVibratorPattern);
+	}
+
+	return pattern;
     }
 
     public boolean getNotifyOnSuccessfulSend() {
@@ -45,11 +65,20 @@ public class Preferences {
     }
 
     public String getDeliverySound() {
-	return sharedPrefs.getString("deliverySound", "DEFAULT_SOUND");
+	return sharedPrefs.getString("deliverySound", DEFAULT_SOUND);
     }
 
-    public String getDeliveryVibratorPattern() {
-	return sharedPrefs.getString("deliveryVibratorPattern", "500");
+    public long[] getDeliveryVibratorPattern() {
+	long[] pattern;
+	try {
+	    pattern = extractPattern(sharedPrefs.getString(DELIVERY_VIBRATOR_PATTERN, DefaultDeliveryVibratorPattern));
+	} catch (Exception e) {
+	    Log.e("OldSchoolSMS", "Wrong vibration pattern: " + e.getLocalizedMessage());
+
+	    pattern = extractPattern(Preferences.DefaultDeliveryVibratorPattern);
+	}
+
+	return pattern;
     }
 
     public String getSmsBox() {
@@ -66,5 +95,25 @@ public class Preferences {
 
     public boolean getExtendedBoxList() {
 	return getDebug() && sharedPrefs.getBoolean("extendedBoxList", false);
+    }
+
+    private long[] extractPattern(String vibratorPattern) {
+	// extract pattern
+	String[] split = vibratorPattern.split("[,.;/\\- ]");
+	// create vibration pattern holder
+	long[] pattern = new long[(split.length + 1)];
+	// set starting wait to 0ms
+	pattern[0] = 0;
+	// decode pattern string
+	for (int i = 0; i < split.length; i++) {
+	    pattern[i + 1] = Long.parseLong(split[i]);
+	}
+	return pattern;
+    }
+
+    public String getLanguage() {
+	String string = sharedPrefs.getString("language", null);
+
+	return string == null ? Locale.getDefault().getCountry() : string;
     }
 }
