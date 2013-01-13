41b5eec050bb19b7feefa62fb8be41bc2c934dd5
kocsis.antal <kocsis.antal@gmail.com>
2013-01-13 16:12:35 +0000

    NEW: SMS lista és SMS nézetből felhívható a szám NEW: SMS nézetből a még ismeretlen szám hozzáadható a telefonkönyvhöz

--- oldSchoolSMS/src/hu/anti/android/oldSchoolSms/SmsViewActivity.java
+++ oldSchoolSMS/src/hu/anti/android/oldSchoolSms/SmsViewActivity.java
@@ -1,7 +1,9 @@
 package hu.anti.android.oldSchoolSms;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog.Builder;
 import android.content.AsyncQueryHandler;
+import android.content.ClipData;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -9,7 +11,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.text.ClipboardManager;
+import android.provider.ContactsContract;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -22,6 +25,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class SmsViewActivity extends AbstractSmsActivity {
+    protected boolean contactExists = false;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,24 +65,29 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	    if (sms == null) {
 		// clear notification
 		try {
-		    NotificationService.removeNotification(this, Integer.parseInt(uri.getLastPathSegment()));
+		    NotificationService.removeNotification(this,
+			    Integer.parseInt(uri.getLastPathSegment()));
 		} catch (NumberFormatException e) {
-		    Log.e("OldSchoolSMS", "SMS id parse error from: " + uri + " message: " + e.getLocalizedMessage());
+		    Log.e("OldSchoolSMS", "SMS id parse error from: " + uri
+			    + " message: " + e.getLocalizedMessage());
 		}
 
 		return;
 	    }
 
 	    final TextView toNumberView = (TextView) findViewById(R.id.textViewNumber);
-	    AsyncQueryHandler asyncQueryHandler = new AsyncQueryHandler(getContentResolver()) {
+	    AsyncQueryHandler asyncQueryHandler = new AsyncQueryHandler(
+		    getContentResolver()) {
 
 		@Override
-		protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+		protected void onQueryComplete(int token, Object cookie,
+			Cursor cursor) {
 		    String personName = null;
 
-		    if (cursor != null && cursor.moveToFirst())
+		    if (cursor != null && cursor.moveToFirst()) {
+			contactExists = true;
 			personName = cursor.getString(0);
-		    else
+		    } else
 			// default...
 			personName = "(" + sms.address + ")";
 
@@ -90,22 +100,29 @@ public class SmsViewActivity extends AbstractSmsActivity {
 		toNumberView.setText(sms.address);
 	    Sms.getDisplayName(asyncQueryHandler, sms.address);
 
-	    setText(R.id.textViewDate, sms.date.toLocaleString());
+	    setText(R.id.textViewDate,
+		    DateFormat.getDateFormat(this).format(sms.date));
 	    setText(R.id.textViewSms, sms.body);
 
-	    if (Sms.Type.MESSAGE_TYPE_SENT.equals(sms.type) || Sms.Type.MESSAGE_TYPE_OUTBOX.equals(sms.type)
-		    || Sms.Type.MESSAGE_TYPE_UNDELIVERED.equals(sms.type) || Sms.Type.MESSAGE_TYPE_FAILED.equals(sms.type)) {
+	    if (Sms.Type.MESSAGE_TYPE_SENT.equals(sms.type)
+		    || Sms.Type.MESSAGE_TYPE_OUTBOX.equals(sms.type)
+		    || Sms.Type.MESSAGE_TYPE_UNDELIVERED.equals(sms.type)
+		    || Sms.Type.MESSAGE_TYPE_FAILED.equals(sms.type)) {
 		// sent
-		setText(R.id.statusText, Sms.decodeSmsDeliveryStatus(getResources(), sms.status));
+		setText(R.id.statusText,
+			Sms.decodeSmsDeliveryStatus(getResources(), sms.status));
 
 		// clear notification
-		NotificationService.removeNotification(this, sms._id.intValue());
+		NotificationService
+			.removeNotification(this, sms._id.intValue());
 	    } else {
 		// received
 		setText(R.id.statusText, "");
 
 		// update notification
-		startService(new Intent(NotificationService.ACTION_UPDATE_SMS_NOTIFICATIONS, null, this, NotificationService.class));
+		startService(new Intent(
+			NotificationService.ACTION_UPDATE_SMS_NOTIFICATIONS,
+			null, this, NotificationService.class));
 	    }
 
 	    // update read flag
@@ -113,7 +130,9 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	    values.put(Sms.Fields.READ, Sms.Other.MESSAGE_IS_READ);
 	    getContentResolver().update(uri, values, null, null);
 	} else {
-	    Toast.makeText(getApplicationContext(), "Received not supported action: " + action, Toast.LENGTH_LONG).show();
+	    Toast.makeText(getApplicationContext(),
+		    "Received not supported action: " + action,
+		    Toast.LENGTH_LONG).show();
 	}
     }
 
@@ -127,6 +146,13 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	inflater.inflate(R.menu.sms, menu);
 
 	return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+	menu.findItem(R.id.saveNumber).setEnabled(!contactExists);
+
+	return super.onPrepareOptionsMenu(menu);
     }
 
     @Override
@@ -144,16 +170,24 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	    // resend only if not a draft
 	    if (!Sms.Type.MESSAGE_TYPE_DRAFT.equals(sms.type)) {
 		Builder resendAlert = createResendAlert();
-		resendAlert.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-		    @Override
-		    public void onClick(DialogInterface dialogInterface, int arg1) {
-			Intent intent = new Intent(NotificationService.ACTION_SEND_SMS, null, SmsViewActivity.this, NotificationService.class);
-			intent.putExtra(NotificationService.EXTRA_ADDRESS, sms.address);
-			intent.putExtra(NotificationService.EXTRA_BODY, sms.body);
+		resendAlert.setPositiveButton(android.R.string.yes,
+			new DialogInterface.OnClickListener() {
+			    @Override
+			    public void onClick(
+				    DialogInterface dialogInterface, int arg1) {
+				Intent intent = new Intent(
+					NotificationService.ACTION_SEND_SMS,
+					null, SmsViewActivity.this,
+					NotificationService.class);
+				intent.putExtra(
+					NotificationService.EXTRA_ADDRESS,
+					sms.address);
+				intent.putExtra(NotificationService.EXTRA_BODY,
+					sms.body);
 
-			startService(intent);
-		    }
-		});
+				startService(intent);
+			    }
+			});
 		resendAlert.show();
 	    }
 
@@ -175,30 +209,61 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	    return true;
 	}
 
-	case R.id.delete:
+	case R.id.delete: {
 	    Builder deletAlert = createDeletAlert();
-	    deletAlert.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-		@Override
-		public void onClick(DialogInterface dialogInterface, int arg1) {
-		    // execute delete
-		    getContentResolver().delete(uri, null, null);
-		    dialogInterface.dismiss();
-		    SmsViewActivity.this.finish();
-		}
-	    });
+	    deletAlert.setPositiveButton(android.R.string.yes,
+		    new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface,
+				int arg1) {
+			    // execute delete
+			    getContentResolver().delete(uri, null, null);
+			    dialogInterface.dismiss();
+			    SmsViewActivity.this.finish();
+			}
+		    });
 	    deletAlert.show();
 
 	    return true;
+	}
 
-	case R.id.copyText:
+	case R.id.copyText: {
 	    Sms sms = Sms.getSms(getContentResolver(), uri);
 	    if (sms == null)
 		return false;
 
-	    ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-	    clipboardManager.setText(sms.body);
+	    if (android.os.Build.VERSION.SDK_INT < 11)
+		copyToClipboard(sms);
+	    else
+		copyToClipboardV11(sms);
 
 	    return true;
+	}
+
+	case R.id.call: {
+	    final Sms sms = Sms.getSms(getContentResolver(), uri);
+	    if (sms == null)
+		return false;
+
+	    Intent intent = new Intent(Intent.ACTION_VIEW);
+	    intent.setData(Uri.parse("tel:" + sms.address));
+	    startActivity(intent);
+
+	    return true;
+	}
+
+	case R.id.saveNumber: {
+	    final Sms sms = Sms.getSms(getContentResolver(), uri);
+	    if (sms == null)
+		return false;
+
+	    Intent intent = new Intent(Intent.ACTION_INSERT_OR_EDIT);
+	    intent.putExtra(ContactsContract.Intents.Insert.PHONE, sms.address);
+	    intent.setType(ContactsContract.Contacts.CONTENT_ITEM_TYPE);
+	    startActivity(intent);
+
+	    return true;
+	}
 
 	case R.id.close:
 	    finish();
@@ -207,6 +272,23 @@ public class SmsViewActivity extends AbstractSmsActivity {
 	default:
 	    return super.onOptionsItemSelected(item);
 	}
+    }
+
+    @SuppressWarnings("deprecation")
+    private void copyToClipboard(final Sms sms) {
+	Log.d("OldSchoolSMS", "copyToClipboard - old version used");
+
+	android.text.ClipboardManager clipboardManager = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	clipboardManager.setText(sms.body);
+    }
+
+    @TargetApi(11)
+    private void copyToClipboardV11(final Sms sms) {
+	Log.d("OldSchoolSMS", "copyToClipboard - API11 version used");
+
+	android.content.ClipboardManager clipboardManager = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+	ClipData clip = ClipData.newPlainText("sms", sms.body);
+	clipboardManager.setPrimaryClip(clip);
     }
 
     /************************************************
